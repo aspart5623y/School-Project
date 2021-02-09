8b61d11698a9d3b80a6a5f9dbb4f38d6c2f17f7d
Williams <willemzy2002@gmail.com>
2021-02-09 15:21:12 -0800

    First Commit

---
+++ admin/admin-js/new-appt.js
@@ -0,0 +1,72 @@
+
+/* ========================  P R E L O A D E R  ======================= */
+
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', () => {
+        document.getElementById('preloader').classList.add('loaded');
+        ready()
+    });
+} else {
+    ready()
+}
+
+
+function ready() {
+    /* ========================  S I D E B A R    T O G G L E R  ======================= */
+    document.querySelector('#navbar-toggler').addEventListener('click', function() {
+        document.querySelector('.admin-panel').classList.toggle('active');
+    });
+   
+
+
+    /* ========================  F O O T E R    D A T E  ======================= */
+    var d = new Date();
+    document.getElementById("footerYear").textContent = d.getFullYear();  
+    
+   
+    document.querySelector('.new1 button').addEventListener('click', () => {
+        document.querySelector('.new1').classList.remove('active')
+        document.querySelector('.new2').classList.add('active')
+    })
+
+    document.querySelector('.new2 button').addEventListener('click', () => {
+        document.querySelector('.new2').classList.remove('active')
+        document.querySelector('.new3').classList.add('active')
+    })
+
+    document.querySelector('.new2 a').addEventListener('click', () => {
+        document.querySelector('.new2').classList.remove('active')
+        document.querySelector('.new1').classList.add('active')
+    })
+
+     document.querySelector('.new3 a').addEventListener('click', () => {
+        document.querySelector('.new3').classList.remove('active')
+        document.querySelector('.new2').classList.add('active')
+    })
+
+
+
+
+
+    
+    /* ========================  A J A X     F O R M     C O N T R O L ======================= */
+    document.getElementById('news').addEventListener('submit', loadDate)
+
+    function loadDate(e) {
+        var xhr = new XMLHttpRequest()
+        xhr.open('GET', 'success.html', true)
+
+        xhr.onload = function () {
+            if (this.status == 200) {
+                // alert("Loading Date Portal")
+                document.querySelector('body').innerHTML = this.responseText
+            }
+        }
+        xhr.send()
+
+        e.preventDefault()
+    }
+
+
+    
+}
